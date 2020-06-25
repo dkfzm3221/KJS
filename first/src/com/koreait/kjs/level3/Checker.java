8911702dae3b7d1c8c50590cf895700951bc3e31
dkfzm <dkfzm@DESKTOP-2QKR6KT>
2020-06-25 22:50:30 +0900

    숙제 실패

--- first/src/com/koreait/kjs/level3/Checker.java
+++ first/src/com/koreait/kjs/level3/Checker.java
@@ -1,24 +1,40 @@
 package com.koreait.kjs.level3;
 
+import com.koreait.kjs.Scan;
+
 public class Checker {
 	// s:0/ b:0 / o:0
 	// 성공하면 flase, 성공 전이면 true
 
-	public static boolean check(int gameCnt, Baseball bb, MyBall mb) {
+	public static boolean check(int gameCnt, Baseball b, MyBall mb) {
+
+		int S = 0;
+		int B = 0;
+		
 
 		while (true) {
-			int s, b = 0;
+
 			for (int i = 0; i < gameCnt; i++) {
 				for (int z = 0; z < gameCnt; z++) {
-					if(i == z) {
-						
+					if (b.get(i) == mb.get(z)) {
+						if (i == z) {
+							S++;
+						} else {
+							B++;
+						}
+
 					}
 
 				}
+				if (S == gameCnt) {
+					break;
+				}
+
 			}
+			
+			System.out.printf("S:%d / B:%d / O:%d\n", S, B, (gameCnt - S - B));
 
 		}
 
 	}
-
 }
--- first/src/com/koreait/kjs/level3/Game.java
+++ first/src/com/koreait/kjs/level3/Game.java
@@ -3,15 +3,15 @@ package com.koreait.kjs.level3;
 public class Game {
 	public static void main(String[] args) {
 		int gameCnt = 3;
-		Baseball ball = new Baseball(4);
-		int val = ball.get(0);
-		
+		Baseball ball = new Baseball(gameCnt);
 		MyBall myBall = new MyBall(gameCnt);
-		myBall.setNumbers();
+		
 		
 		do {
 			myBall.setNumbers();
 		}while(Checker.check(gameCnt, ball, myBall));
+		
+		System.out.println("게임 종료!");
 		
 		//숫자1:3
 	}
